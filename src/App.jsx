ed3d065b23386cfb1f8b1396d434c5dc53594b77
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-06 10:04:34 +0530

    Refactor dark mode initialization logic to correctly read localStorage value

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") === "false");
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") === "true");
+  console.log(darkMode)
   return (
     <div className={`${darkMode ? 'dark' : ''} text-foreground bg-background`}>
       <Navbar setDarkMode={setDarkMode} darkMode={darkMode} />
